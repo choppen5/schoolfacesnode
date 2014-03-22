6c33f1ff7830672b3f5884aed23568eacfdd49d8
choppen5 <choppen5@yahoo.com>
2014-03-21 21:51:12 -0700

    initial commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+	'url' : 'mongodb://heroku:eiFVs6YF8nTj911_2NXE9iixo7ZoqM377XQz6zmm-nSLg-5x1lDDlqNFLTAqDGJCI1OGx2cDnhME6CzVRmNdiw@oceanic.mongohq.com:10006/app23250731' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
