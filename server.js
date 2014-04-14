5a41834206a8c3a7a88e0772d312e733c3d79ad2
choppen5 <choppen5@yahoo.com>
2014-04-13 22:48:28 -0700

    node 4 upgrade

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ var port     = process.env.PORT || 8080;
 var mongoose = require('mongoose');
 var passport = require('passport');
 var flash 	 = require('connect-flash');
+var logger = require('morgan');
+var cookieParser = require('cookie-session');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var configDB = require('./config/database.js');
 
@@ -16,22 +20,25 @@ mongoose.connect(configDB.url); // connect to our database
 
 require('./config/passport')(passport); // pass passport for configuration
 
-app.configure(function() {
+//app.configure(function() {
 
 	// set up our express application
-	app.use(express.logger('dev')); // log every request to the console
-	app.use(express.cookieParser()); // read cookies (needed for auth)
-	app.use(express.bodyParser()); // get information from html forms
+	app.use(logger('dev')); // log every request to the console
+	app.use(cookieParser({
+  		keys: ['a', 'b']
+	})); // read cookies (needed for auth)
+	
+	app.use(bodyParser()); // get information from html forms
 
 	app.set('view engine', 'ejs'); // set up ejs for templating
 
 	// required for passport
-	app.use(express.session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+	app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
 	app.use(passport.initialize());
 	app.use(passport.session()); // persistent login sessions
 	app.use(flash()); // use connect-flash for flash messages stored in session
 
-});
+//});
 
 // routes ======================================================================
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
